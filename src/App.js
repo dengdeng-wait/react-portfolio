748944e968557f95c862cfc45a04a142d3862347
tora20 <tora20@m4a.co.kr>
2024-01-08 10:39:20 +0900

    hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import './styles/Common.css';
 import styled from 'styled-components';
 import Navi from './component/Navi';
@@ -15,18 +15,19 @@ const App = () => {
 
   return (
     <div className='wrap bg-gray-100 dark:bg-slate-900'>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      {/* browserrouter github배포에러 그래서 hashrouter로 변경/ 일반배포는 browserrouter 사용함 */}
+      <HashRouter>
         <div className="p-4 pb-12">
           <Navi years={years} />
           <Routes>
             <Route path="/" element={<Contents />}></Route>
             <Route path="/Contents/:years" element={<Contents />}></Route>
             {/* 상단 위치 라우트 규칙 모두확인 일치 라우트 없는경우 처리 */}
-            <Route path="/*" element={<NotFound />}></Route>
+            <Route path="*" element={<NotFound />}></Route>
           </Routes>
           <MoveToUp></MoveToUp>
         </div>
-      </BrowserRouter>
+      </HashRouter>
       {/* <div>데이터: {JSON.stringify(data)}</div> */}
       
     </div>
