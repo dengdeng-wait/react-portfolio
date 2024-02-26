ba06369d223f3e455e8fdef072dc60d402ed127f
tora20 <tora20@m4a.co.kr>
2024-02-26 13:54:11 +0900

    json 메뉴반영

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
-import React from 'react';
-import { HashRouter, Routes, Route } from 'react-router-dom';
-import './styles/Common.css';
-import styled from 'styled-components';
-import Navi from './component/Navi';
-import Contents from './component/Contents';
-import NotFound from './component/NotFound';
-import MoveToUp from './component/MoveToUp';
-import Loading from './component/Loading';
+import React from "react";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import "./styles/Common.css";
+import styled from "styled-components";
+import Navi from "./component/Navi";
+import Contents from "./component/Contents";
+import NotFound from "./component/NotFound";
+import MoveToUp from "./component/MoveToUp";
+import Loading from "./component/Loading";
 
 const App = () => {
   // const [mode, setMode] = useState(true);
 
-  const years = ['2023-2021', '2020-2019', '2018-2017', '2016-2015', '2014-2013', '2012-2011', '2010이전'];
+  const years = [
+    "2024",
+    "2023-2021",
+    "2020-2019",
+    "2018-2017",
+    "2016-2015",
+    "2014-2013",
+    "2012-2011",
+    "2010이전",
+  ];
 
   return (
-    <div className='wrap bg-gray-100 dark:bg-slate-900'>
+    <div className="wrap bg-gray-100 dark:bg-slate-900">
       {/* browserrouter github배포에러 그래서 hashrouter로 변경/ 일반배포는 browserrouter 사용함 */}
       <HashRouter>
         <div className="p-4 pb-12">
@@ -29,9 +38,8 @@ const App = () => {
         </div>
       </HashRouter>
       {/* <div>데이터: {JSON.stringify(data)}</div> */}
-      
     </div>
   );
-}
+};
 
 export default App;
